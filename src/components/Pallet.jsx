e988de09652c7f31e969dcca7ea6ff9e02e88650
Alex Pitsikoulis <alex.pitsikoulis@gmail.com>
2019-10-24 12:05:30 -0400

    added functionality to allow users to select from their 3 most recently used colors

--- src/components/Pallet.jsx
+++ src/components/Pallet.jsx
@@ -2,15 +2,17 @@ import React, { useState } from "react";
 
 export default function Pallet(props) {
 	const [colorChoice, setColorChoice] = useState(props.color);
+	const [previousColors, setPreviousColors] = useState([]);
+
+	if (previousColors.length > 3) {
+		setPreviousColors([...previousColors].slice(1));
+	}
 
 	const palletStyles = {
 		height: "100px",
 		width: "100px",
-		background: props.color,
-		margin: "5px auto"
+		background: props.color
 	};
-
-	let previousColors
 
 	function handleInputChange(event) {
 		let copiedColorChoice = { colorChoice };
@@ -19,12 +21,36 @@ export default function Pallet(props) {
 	}
 
 	function handleColorChange() {
+		setPreviousColors([...previousColors].concat(props.color));
 		props.setColor(colorChoice);
 	}
 
+	function selectPreviousColor(event) {
+		setPreviousColors([...previousColors].concat(props.color));
+		props.setColor(event.target.className.split(" ")[1]);
+	}
+
+	const prevPallets = previousColors.map((prev, i) => {
+		return (
+			<div
+				className={`previous ${prev}`}
+				key={i}
+				style={{
+					height: "20px",
+					width: "20px",
+					background: prev,
+					margin: "5px"
+				}}
+				onClick={selectPreviousColor}></div>
+		);
+	});
+
 	return (
 		<div>
-			<div style={palletStyles} id='pallet'></div>
+			<div id='swatches'>
+				{prevPallets}
+				<div style={palletStyles} id='pallet'></div>
+			</div>
 			<input
 				type='text'
 				id='colorPicker'
